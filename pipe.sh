2b60d739e9f15cd9dea80b464b7882b42dcce10f
juliyen <79110719+juliyen@users.noreply.github.com>
2022-01-05 12:56:15 +0100

    Update pipe.sh

--- pipe.sh
+++ pipe.sh
@@ -10,6 +10,8 @@ cut -f1 luscinia_vars.vcf | grep -v "^#" > chr
 
  ##merging these lists 
 paste chr DP | cut -f1,2 > ready_to_work.csv
+ ## removing trash
+grep -v "random\|chrAmb\|chrL\|chrUn" ready_to_work.csv | sort  > ready_to_work.csv
 
  ##Great!
 echo 'Success'
